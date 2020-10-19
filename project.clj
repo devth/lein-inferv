61dad9a39fb57dd8a6ce7fff98db244c6179416e
Trevor Hartman <trevorhartman@gmail.com>
2020-10-18 21:43:51 -0600

    Switch to LATEST

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-inferv "0.1.0-SNAPSHOT"
+(defproject lein-inferv "_"
   :description "Opinionated inferred versions as a lein plugin"
   :url "https://github.com/devth/lein-inferv"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-inferv "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-inferv "LATEST"]]
   :eval-in-leiningen true)
